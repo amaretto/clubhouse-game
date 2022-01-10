176efccfe63ca8a64ca516071a0d5076eee85b34
amaretto <jabberwock26@gmail.com>
2022-01-10 12:31:46 +0900

    finish implement all tentative functions

--- blackjack/main.go
+++ blackjack/main.go
@@ -17,14 +17,23 @@ var (
 )
 
 type Player struct {
+	name   string
 	hand   []int
 	result int
 	chip   int
 }
 
+type Dealer struct {
+	Player
+}
+
 type Deck struct {
 	n     int
 	cards []int
+}
+
+type Game struct {
+	round int
 }
 
 func main() {
@@ -59,43 +68,57 @@ func playGame(cards []int) {
 	n = 4
 
 	// notify info
-	fmt.Println("\n\n/////Dealer/////")
-	fmt.Printf("%d,*\n", d.hand[0])
+	fmt.Printf("\x1b[32m")
+	fmt.Printf("///////////////////////////////\n")
+	fmt.Printf("//////////start game!//////////\n")
+	fmt.Printf("///////////////////////////////\n")
+	fmt.Printf("\x1b[0m")
 
-	fmt.Println("/////You/////")
-	fmt.Println(join(p.hand))
-	fmt.Printf("Total : %d\n\n", countTotal(p.hand))
+	fmt.Printf("\x1b[31m")
+	fmt.Println("\n/////Dealer Hand/////")
+	fmt.Printf("Cards: %d,*\n", d.hand[0])
+	fmt.Printf("\x1b[0m")
+
+	fmt.Printf("\x1b[34m")
+	fmt.Println("\n//////Your Hand//////")
+	fmt.Println("Cards: " + joinHands(p.hand))
+	fmt.Printf("Total: %d\n\n", countTotal(p.hand))
 
 	// player turn
+	var input string
+	fmt.Println("\n//////Your Turn!!//////\n")
 	for {
 		fmt.Println("Hit(h)/Stand(s)?")
-		var input string
 		fmt.Scan(&input)
 		if input == "h" {
-			fmt.Println("card: ", cards[n])
+			fmt.Println("New Card: ", cards[n])
 			p.hand = append(p.hand, cards[n])
 			n++
+			fmt.Println("Your Hands: ", joinHands(p.hand))
 			// do judge
 			pj := judgeHand(countTotal(p.hand))
 			if pj == 0 {
-				fmt.Println("Blackjack!")
+				fmt.Println("Player Total: Blackjack!!!")
 				p.result = 21
 				break
 			} else if pj == 1 {
+				fmt.Println("Player Total:", countTotal(p.hand))
 				continue
 			} else {
-				fmt.Println("Burst!!")
+				fmt.Println("Player Total: Bursted...")
 				p.result = minCount(countTotal(p.hand))
 				break
 			}
 		} else if input == "s" {
 			p.result = maxCount(countTotal(p.hand))
+			fmt.Println("\nPlayer Total:", p.result)
 			break
 		} else {
 			fmt.Println("invalid input")
 			continue
 		}
 	}
+	fmt.Printf("\x1b[0m")
 
 	// dealer turn
 	dealerProcess(cards, &d)
@@ -109,14 +132,20 @@ func playerProcess(cards []int, p Player) {
 }
 
 func dealerProcess(cards []int, d *Player) {
-	fmt.Println("\n\n------Dealer Turn------")
+	fmt.Printf("\x1b[31m")
+	fmt.Println("\n\n//////Dealer Turn!//////")
 
 	for {
+		fmt.Println("Delaer Hand:", joinHands(d.hand))
+		time.Sleep(1 * time.Second)
 		currentMax := maxCount(countTotal(d.hand))
 		if currentMax >= 17 {
 			d.result = currentMax
+			fmt.Println("Dealer Total:", d.result)
+			fmt.Printf("\x1b[0m")
 			return
 		}
+		fmt.Println("New Card: ", cards[n])
 		d.hand = append(d.hand, cards[n])
 		n++
 	}
@@ -147,7 +176,11 @@ func minCount(counts []int) int {
 }
 
 func judgeResult(p, d Player) {
-	fmt.Printf("Player:%d, Dealer:%d\n", p.result, d.result)
+	time.Sleep(1 * time.Second)
+	fmt.Println("\n\n//////   Judge!   //////\n")
+	time.Sleep(1 * time.Second)
+	fmt.Printf("Player:%d, Dealer:%d\n\n", p.result, d.result)
+	time.Sleep(1 * time.Second)
 	if p.result > 21 && d.result > 21 || p.result == d.result {
 		fmt.Println("Draw")
 	} else if p.result <= 21 && (p.result > d.result || d.result > 21) {
@@ -182,6 +215,7 @@ func countTotal(hands []int) []int {
 	} else {
 		result = append(result, 10)
 	}
+
 	for i := 1; i < len(hands); i++ {
 		if hands[i] == 1 {
 			tmp := []int{}
@@ -202,14 +236,29 @@ func countTotal(hands []int) []int {
 			}
 		}
 	}
-	fmt.Println("Total:", join(result))
 	return result
 }
 
-func join(cards []int) string {
-	result := strconv.Itoa(cards[0])
-	for i := 1; i < len(cards); i++ {
-		result += ", " + strconv.Itoa(cards[i])
+func joinHands(cards []int) string {
+	var result string
+	for i := 0; i < len(cards); i++ {
+		var tmp string
+		if cards[i] < 11 {
+			tmp = strconv.Itoa(cards[i])
+		} else {
+			if cards[i] == 11 {
+				tmp = "J"
+			} else if cards[i] == 12 {
+				tmp = "Q"
+			} else {
+				tmp = "K"
+			}
+		}
+		if result == "" {
+			result = tmp
+		} else {
+			result += ", " + tmp
+		}
 	}
 	return result
 }
