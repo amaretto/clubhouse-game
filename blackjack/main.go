b7899f24a341e1aef21e8cca1e50218e00059cc3
amaretto <jabberwock26@gmail.com>
2022-01-21 23:22:11 +0900

    continue to implement UI

--- blackjack/main.go
+++ blackjack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/rivo/tview"
 )
@@ -18,6 +19,7 @@ const (
 type Game struct {
 	app *tview.Application
 	ui  *tview.Flex
+	ui2 *tview.TextView
 
 	dealer   Dealer
 	players  []Player
@@ -34,25 +36,37 @@ func (g *Game) playGame() {
 
 	delay()
 
-	for i := 0; i < g.roundNum; i++ {
-		fmt.Printf("\x1b[32m")
-		fmt.Printf("\n\n----- Round %d -----\n", i+1)
-		fmt.Printf("\x1b[0m")
-		g.deck.shuffle()
-		g.round()
+	g.ui2.SetBorder(true)
+	g.ui.AddItem(g.ui2, 0, 1, false)
+	go func() {
+		for {
+			g.app.Draw()
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+	if err := g.app.SetRoot(g.ui, true).Run(); err != nil {
+		panic(err)
 	}
 
-	var p *Player
-	for i := 0; i < len(g.players); i++ {
-		p = &g.players[i]
-		if p.chip > 1000 {
-			fmt.Printf("Player %s won $%d\n", p.name, p.chip-1000)
-		} else if p.chip == 1000 {
-			fmt.Printf("Player %s chip don't change\n", p.name)
-		} else {
-			fmt.Printf("Player %s lose $%d\n", p.name, 1000-p.chip)
-		}
-	}
+	//for i := 0; i < g.roundNum; i++ {
+	//	fmt.Printf("\x1b[32m")
+	//	fmt.Printf("\n\n----- Round %d -----\n", i+1)
+	//	fmt.Printf("\x1b[0m")
+	//	g.deck.shuffle()
+	//	g.round()
+	//}
+
+	//var p *Player
+	//for i := 0; i < len(g.players); i++ {
+	//	p = &g.players[i]
+	//	if p.chip > 1000 {
+	//		fmt.Printf("Player %s won $%d\n", p.name, p.chip-1000)
+	//	} else if p.chip == 1000 {
+	//		fmt.Printf("Player %s chip don't change\n", p.name)
+	//	} else {
+	//		fmt.Printf("Player %s lose $%d\n", p.name, 1000-p.chip)
+	//	}
+	//}
 }
 
 func (g *Game) round() {
@@ -69,7 +83,7 @@ func (g *Game) round() {
 }
 
 func main() {
-	g := Game{}
+	g := Game{app: tview.NewApplication(), ui: tview.NewFlex(), ui2: tview.NewTextView()}
 	g.dealer = Dealer{Player{name: "Dealer", chip: 1000000}, &g}
 	g.players = []Player{Player{name: "A", chip: 1000}}
 
