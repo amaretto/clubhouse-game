36bd1b15ebf3a73b9803e67b0addcd197a9e09d1
amaretto <jabberwock26@gmail.com>
2022-01-23 22:47:46 +0900

    add struct for UI

--- blackjack/ui.go
+++ blackjack/ui.go
@@ -5,3 +5,23 @@ import "github.com/rivo/tview"
 type UI struct {
 	*tview.Flex
 }
+
+type DealerUI struct {
+	*tview.Flex
+	hand HandView
+	chip ChipView
+}
+
+type PlayerUI struct {
+	*tview.Flex
+	hand HandView
+	chip ChipView
+}
+
+type HandView struct {
+	*tview.TextView
+}
+
+type ChipView struct {
+	*tview.TextView
+}
